ee2d488f861567268c3c66201c62371f42c0025b
kilg <apomazkin@gmail.com>
2020-11-22 02:51:09 +0500

    I001 PartOfSpeech: options

--- feature/feature-vocabulary-impl/src/main/java/me/apomazkin/feature_vocabulary_impl/ui/addDefinition/AddDefinitionViewModel.kt
+++ feature/feature-vocabulary-impl/src/main/java/me/apomazkin/feature_vocabulary_impl/ui/addDefinition/AddDefinitionViewModel.kt
@@ -1,5 +1,8 @@
 package me.apomazkin.feature_vocabulary_impl.ui.addDefinition
 
+import android.util.Log
+import android.view.View
+import androidx.databinding.ObservableField
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import me.apomazkin.core_db_api.CoreDbApi
@@ -13,6 +16,12 @@ class AddDefinitionViewModel(
 ) : ViewModel() {
 
     val addingDefinition = MutableLiveData<String>()
+    val nounCountableVisibility1 = MutableLiveData<Int>()
+    val nounCountableVisibility = ObservableField<Int>(View.GONE)
+    val verbTransitiveVisibility = ObservableField<Int>(View.GONE)
+    val nounCountableChecked = MutableLiveData<Boolean>(false)
+    val verbTransitiveChecked = MutableLiveData<Boolean>(false)
+
 
     // TODO: 26.10.2020 заменить стрингу на sealed class.
     //  1. Для этого добавить в sealed дефолтные значения undefined.
@@ -22,29 +31,43 @@ class AddDefinitionViewModel(
 
     fun onNounSelect() {
         partOfSpeech.postValue("noun")
+        nounCountableVisibility.set(View.VISIBLE)
+        verbTransitiveVisibility.set(View.GONE)
+//        nounCountableVisibility.postValue(View.VISIBLE)
     }
 
     fun onVerbSelect() {
         partOfSpeech.postValue("verb")
+        nounCountableVisibility.set(View.GONE)
+        verbTransitiveVisibility.set(View.VISIBLE)
+//        nounCountableVisibility.postValue(View.GONE)
     }
 
     fun onAdjectiveSelect() {
         partOfSpeech.postValue("adjective")
+        nounCountableVisibility.set(View.GONE)
+        verbTransitiveVisibility.set(View.GONE)
+//        nounCountableVisibility.postValue(View.GONE)
     }
 
     fun onAdverbSelect() {
         partOfSpeech.postValue("adverb")
+        nounCountableVisibility.set(View.GONE)
+        verbTransitiveVisibility.set(View.GONE)
+//        nounCountableVisibility.postValue(View.GONE)
     }
 
     fun onAddDefinition() {
+        Log.d("###", "noun: ${nounCountableChecked.value}")
+        Log.d("###", "verb: ${verbTransitiveChecked.value}")
         addingDefinition.value?.let {
             dbApi.addDefinition(
                 Definition(
                     wordId = id,
                     definition = it,
                     partOfSpeech = when (partOfSpeech.value) {
-                        "noun" -> Noun(null)
-                        "verb" -> Verb(null)
+                        "noun" -> Noun(nounCountableChecked.value)
+                        "verb" -> Verb(verbTransitiveChecked.value)
                         "adjective" -> Adjective
                         "adverb" -> Adverb
                         else -> null
