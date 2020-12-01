742c3606b219aec9253f9fd06cdd0de8cb1c2906
kilg <apomazkin@gmail.com>
2020-12-02 01:41:03 +0500

    IS021 Keep WordClass options as flags

--- core/core-db-api/src/main/java/me/apomazkin/core_db_api/entity/WordClass.kt
+++
@@ -1,7 +0,0 @@
-package me.apomazkin.core_db_api.entity
-
-sealed class WordClass
-data class Verb(val isTransitive: Boolean?) : WordClass()
-data class Noun(val isCountable: Boolean?) : WordClass()
-object Adjective : WordClass()
-object Adverb : WordClass()
