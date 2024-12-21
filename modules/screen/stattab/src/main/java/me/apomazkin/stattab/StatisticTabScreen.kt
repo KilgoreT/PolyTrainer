e48a499a3b798c098f2814e86a591dc0e692850a
apomazkin <apomazkin@gmail.com>
2024-12-21 02:56:59 -0300

    IS303. Added Tabs Screens.

---
+++ modules/screen/stattab/src/main/java/me/apomazkin/stattab/StatisticTabScreen.kt
@@ -0,0 +1,20 @@
+package me.apomazkin.stattab
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun StatisticTabScreen(
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Yellow)
+    ) {
+
+    }
+}
