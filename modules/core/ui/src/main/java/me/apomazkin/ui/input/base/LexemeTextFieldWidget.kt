9ca253fec2efe923cf512e86e506dee01bde4ce1
apomazkin <apomazkin@gmail.com>
2025-06-16 20:02:10 -0600

    IS321. Fixed dev build on feature push.

--- modules/core/ui/src/main/java/me/apomazkin/ui/input/base/LexemeTextFieldWidget.kt
+++ modules/core/ui/src/main/java/me/apomazkin/ui/input/base/LexemeTextFieldWidget.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import me.apomazkin.theme.LexemeStyle
@@ -34,68 +35,69 @@ import me.apomazkin.ui.R
 // TODO: Добавить хинт, подсказку
 @Composable
 fun LexemeTextFieldWidget(
-    modifier: Modifier = Modifier,
-    imeAction: ImeAction = ImeAction.Default,
-    autoCorrect: Boolean = true,
-    erasable: Boolean = false,
-    @StringRes placeHolder: Int?,
-    isInputEnabled: Boolean = true,
-    value: String,
-    onValueChange: (String) -> Unit,
-    onKeyboardActions: () -> Unit,
+        modifier: Modifier = Modifier,
+        imeAction: ImeAction = ImeAction.Default,
+        autoCorrect: Boolean = true,
+        erasable: Boolean = false,
+        @StringRes placeHolder: Int?,
+        isInputEnabled: Boolean = true,
+        value: String,
+        onValueChange: (String) -> Unit,
+        onKeyboardActions: () -> Unit,
 ) {
     var textFieldValue by remember {
         mutableStateOf(
-            TextFieldValue(
-                text = value,
-                selection = TextRange(value.length)
-            )
+                TextFieldValue(
+                        text = value,
+                        selection = TextRange(value.length)
+                )
         )
     }
     LaunchedEffect(value) {
         textFieldValue = textFieldValue.copy(
-            text = value,
-            selection = TextRange(value.length)
+                text = value,
+                selection = TextRange(value.length)
         )
     }
-    
+
     OutlinedTextField(
-        modifier = modifier,
-        enabled = isInputEnabled,
-        value = textFieldValue,
-        onValueChange = {
-            textFieldValue = it
-            onValueChange.invoke(it.text)
-        },
-        trailingIcon = {
-            if (erasable && value.isNotBlank()) {
-                Icon(
-                    modifier = Modifier
-                        .clickable { onValueChange.invoke("") },
-                    painter = painterResource(id = R.drawable.ic_clear),
-                    contentDescription = ""
-                )
-            }
-        },
-        placeholder = {
-            placeHolder?.let {
-                Text(
-                    text = stringResource(id = it),
-                    style = LexemeStyle.BodyM.copy(color = grayTextColor)
-                )
-            }
-        },
-        singleLine = false,
-        textStyle = LexemeStyle.BodyL,
-        shape = RoundedCornerShape(0.dp),
-        keyboardOptions = KeyboardOptions(
-            autoCorrectEnabled = autoCorrect,
-            imeAction = imeAction
-        ),
-        colors = OutlinedTextFieldDefaults.colors(
-            focusedBorderColor = Color.Transparent,
-            unfocusedBorderColor = Color.Transparent,
-        ),
-        keyboardActions = KeyboardActions { onKeyboardActions.invoke() }
+            modifier = modifier,
+            enabled = isInputEnabled,
+            value = textFieldValue,
+            onValueChange = {
+                textFieldValue = it
+                onValueChange.invoke(it.text)
+            },
+            trailingIcon = {
+                if (erasable && value.isNotBlank()) {
+                    Icon(
+                            modifier = Modifier
+                                    .clickable { onValueChange.invoke("") },
+                            painter = painterResource(id = R.drawable.ic_clear),
+                            contentDescription = ""
+                    )
+                }
+            },
+            placeholder = {
+                placeHolder?.let {
+                    Text(
+                            text = stringResource(id = it),
+                            style = LexemeStyle.BodyM.copy(color = grayTextColor)
+                    )
+                }
+            },
+            singleLine = false,
+            textStyle = LexemeStyle.BodyL,
+            shape = RoundedCornerShape(0.dp),
+            keyboardOptions = KeyboardOptions(
+                    keyboardType = if (autoCorrect) KeyboardType.Unspecified else KeyboardType.Password,
+                    autoCorrectEnabled = autoCorrect,
+                    imeAction = imeAction
+            ),
+            colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = Color.Transparent,
+                    unfocusedBorderColor = Color.Transparent,
+            ),
+            keyboardActions = KeyboardActions { onKeyboardActions.invoke() }
     )
 }
