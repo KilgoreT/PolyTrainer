82c8ee29381ce487b655b6e5b4f2cf86074642c6
apomazkin <apomazkin@gmail.com>
2025-06-17 21:07:55 -0600

    IS388. Added chat button ripple.

--- modules/screen/quiz/chat/src/main/java/me/apomazkin/quiz/chat/widget/button/base/ChatButtonWidget.kt
+++ modules/screen/quiz/chat/src/main/java/me/apomazkin/quiz/chat/widget/button/base/ChatButtonWidget.kt
@@ -4,6 +4,7 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -12,7 +13,9 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,10 +33,20 @@ fun ChatButtonWidget(
     @StringRes title: Int,
     onClick: () -> Unit,
 ) {
+
+    val interactionSource = remember { MutableInteractionSource() }
+
     Surface(
         modifier = Modifier
             .height(48.dp)
-            .clickable { onClick.invoke() },
+            .clickable(
+                    interactionSource = interactionSource,
+                    indication = ripple(
+                        bounded = false,
+                        radius = 56.dp,
+                        color = Color.Red,
+                    ),
+            ) { onClick.invoke() },
         shape = RoundedCornerShape(
             topStart = 20.dp,
             topEnd = 20.dp,
